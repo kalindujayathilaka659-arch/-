1884b66b7896a47eea79f5bfe137468268b546b2
kalindujayathilaka659-arch <kalindujayathilaka659@gmail.com>
2026-01-04 17:52:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "cZ4ygCyY#hbYEpd4eCAH7WzTUSbVMxXHNwo7gUt1mjewr8g-TvFw",
+  SESSION_ID: process.env.SESSION_ID || "KEkljS6b#Bksgu4HFB7CdwK_sYE_mDrXplefWV-qQOc6VOMHIjtI",
 
-  MONGODB: process.env.MONGODB || "mongodb://mongo:LxUYYgUicUbyasODhinZfhHLqnaBMtAQ@yamabiko.proxy.rlwy.net:46053",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:XenHeRDUjMLxafGOvMuPVNoSEwqdNCPo@tramway.proxy.rlwy.net:39180",
 
   OWNER_NUM: (process.env.OWNER_NUM || "94701981053").split(","),
 
-  MODE: process.env.MODE || "group",
+  MODE: (process.env.MODE || "group"),
 
   ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nadeelachamath-crypto/GHOST-SUPPORT/blob/main/ChatGPT%20Image%20Oct%2031,%202025,%2010_10_49%20PM.png?raw=true"
 };
