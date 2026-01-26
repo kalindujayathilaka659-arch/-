12691d53f5d324b7f995599907f357362b5b8dcd
kalindujayathilaka659-arch <kalindujayathilaka659@gmail.com>
2026-01-26 21:47:01 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -67,8 +67,6 @@ cmd(
     👻 ${config.PREFIX}mega <mrga.nz url>
     👻 ${config.PREFIX}download <direct download url>
     👻 ${config.PREFIX}torrent <torrent magnet url>
-    👻 ${config.PREFIX}largefile <torrent magnet url>
-    
     
 | _*SOCIAL MEDIA DOWNLOAD COMMANDS*_ |
     👻 ${config.PREFIX}song <song name>
